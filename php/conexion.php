775ac3dc03adc0e7f1eaf78d09122cd3195ffc8f
rnovoa89 <renenovoaa89@gmail.com>
2024-10-13 19:20:21 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -10,12 +10,16 @@ $dbname = "tienda_online";
 
 //Eliminar esto despues de haberlo pasado a Rene para lo de Azure y dejar solamente la cadena para la DB local
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "SSl/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
-    die('Conexión fallida: Con MySQL de Azure: '.mysqli_connect_error());
-}
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "tiendaenlinea-server.mysql.database.azure.com", "ushfdkwvxu", "Tiendaenlinea2024", "tienda_online", 3306, MYSQLI_CLIENT_SSL);
+
+//$conn = mysqli_init();
+//mysqli_ssl_set($conn,NULL,NULL, "SSl/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+//mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+//if (mysqli_connect_errno()) {
+    //die('Conexión fallida: Con MySQL de Azure: '.mysqli_connect_error());
+//}
 
 // Verificar conexión
 if ($conn->connect_error) {
