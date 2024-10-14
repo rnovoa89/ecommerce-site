1f0a42feefb64897647c67890913dc987bc08e62
Romeo Isaac Pérez Flores <119085223+WhiteStrom@users.noreply.github.com>
2024-10-13 20:02:52 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -4,7 +4,7 @@ $servername = "tiendaenlinea-server.mysql.database.azure.com";
 $username = "ushfdkwvxu";
 $password = "Tindaenlinea2024";
 $dbname = "tienda_online";
-$ssl_cert_path = "__DIR__ ./SSL/DigiCertGlobalRootCA.crt.pem";  // Ruta completa al certificado
+$ssl_cert_path = __DIR__ . "/SSL/DigiCertGlobalRootCA.crt.pem";  // Ruta completa al certificado
 
 // Crear la conexión
 $conn = mysqli_init();
@@ -14,11 +14,6 @@ mysqli_ssl_set($conn, NULL, NULL, $ssl_cert_path, NULL, NULL);
 
 // Realizar la conexión a la base de datos
 if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
-    die(json_encode(['success' => false, 'error' => "Conexión fallida: " . mysqli_connect_error()]));
-}
-
-// Verificar la conexión
-if (mysqli_connect_errno()) {
     die(json_encode(['success' => false, 'error' => "Conexión fallida: " . mysqli_connect_error()]));
 }
 
