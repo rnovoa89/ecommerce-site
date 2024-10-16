bc8aff3515120a3d818f35361c1ac8f944a48440
Romeo Isaac Pérez Flores <119085223+WhiteStrom@users.noreply.github.com>
2024-10-16 09:32:39 -0600

    Update register_admin.php

--- php/register_admin.php
+++ php/register_admin.php
@@ -1,8 +1,8 @@
 <?php
 // Datos de conexión a la base de datos
-$servername = "ecommerce2k24-server.mysql.database.azure.com";
-$username = "gxnxubadse";
-$password = "qKDe0VUjZ$2hTrW9";
+$servername = "tiendaenlinea-server.mysql.database.azure.com";
+$username = "ushfdkwvxu";
+$password = "Tindaonline2024";
 $dbname = "tienda_online";
 
 // Habilitar reportes de errores de MySQLi para depuración
