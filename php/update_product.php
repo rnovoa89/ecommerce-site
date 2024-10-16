88eec21c25ba35d5ddfce67c053a9e58a4ee9d56
rnovoa89 <renenovoaa89@gmail.com>
2024-10-15 19:43:54 -0600

    Update update_product.php

--- php/update_product.php
+++ php/update_product.php
@@ -1,12 +1,27 @@
 <?php
 header('Content-Type: application/json');
 
-// Conexión a la base de datos
-include 'conexion.php';
+// Datos de conexión a la base de datos
+$servername = "tiendaenlinea-server.mysql.database.azure.com";
+$username = "ushfdkwvxu";
+$password = "Tindaonline2024";
+$dbname = "tienda_online";
 
-// Verificar conexión
-if ($conn->connect_error) {
-    echo json_encode(['success' => false, 'error' => "Conexión fallida: " . $conn->connect_error]);
+// Habilitar reportes de errores de MySQLi para depuración
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// Crear la conexión
+$conn = mysqli_init();
+
+// Verificar si la configuración SSL se establece correctamente
+if (!mysqli_ssl_set($conn, NULL, NULL, __DIR__ . "/../SSL/DigiCertGlobalRootCA.crt.pem", NULL, NULL)) {
+    echo json_encode(['success' => false, 'error' => "Falló la configuración SSL"]);
+    exit();
+}
+
+// Realizar la conexión a la base de datos
+if (!mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    echo json_encode(['success' => false, 'error' => "Conexión fallida: " . mysqli_connect_error()]);
     exit();
 }
 
@@ -14,7 +29,7 @@ if ($conn->connect_error) {
 $data = json_decode(file_get_contents("php://input"), true);
 
 // Asegurarse de que se envió un ID y los nuevos datos
-if (isset($data['id']) && isset($data['nombre']) && isset($data['precio']) && isset($data['descripcion']) && isset($data['imagen']) ) {
+if (isset($data['id']) && isset($data['nombre']) && isset($data['precio']) && isset($data['descripcion']) && isset($data['imagen'])) {
     $id = $conn->real_escape_string($data['id']);
     $nombre = $conn->real_escape_string($data['nombre']);
     $precio = $conn->real_escape_string($data['precio']);
